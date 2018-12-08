4be92d3a9e16bdb2ff614e0bb0000b47a706f9a9
Ripal Patel <ripal212007@gmail.com>
2018-12-08 09:51:26 -0600

    completed styles for contacts and projects

--- src/javascripts/data/projectData.js
+++ src/javascripts/data/projectData.js
@@ -14,6 +14,7 @@ const getAllProjectsFromDb = () => new Promise((resolve, reject) => {
         Object.keys(allProjectsObject).forEach((projectId) => {
           const newProject = allProjectsObject[projectId];
           newProject.id = projectId;
+          console.log(projectId);
           allProjectsArray.push(newProject);
         });
       }
